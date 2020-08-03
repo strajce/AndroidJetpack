06916dd6365d5a84dd106fc0d90f833cd127081a
s.vuckovic <s.vuckovic@levi9.com>
2020-08-03 14:16:22 +0200

    navigation

---
+++ app/src/main/java/com/example/dogs/view/DetailsFragment.kt
@@ -0,0 +1,34 @@
+package com.example.dogs.view
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.navigation.Navigation
+import com.example.dogs.R
+import kotlinx.android.synthetic.main.fragment_details.*
+
+class DetailsFragment : Fragment() {
+
+    private var dogUuid = 0
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_details, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        arguments?.let {
+            dogUuid = DetailsFragmentArgs.fromBundle(it).dogUuid
+        }
+
+        btn_list.setOnClickListener {
+            Navigation.findNavController(it).navigate(DetailsFragmentDirections.detailsFragmentToListFragment())
+        }
+    }
+}
